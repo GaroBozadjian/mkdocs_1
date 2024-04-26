50443ad665ab55bce3d36f3da8b633163eb95b97
GaroBozadjian <garobozadjian@hotmail.com>
2024-04-26 08:46:41 +0400

    mkdocs test

---
+++ calculator/main.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    """
+    Add two numbers
+
+    Examples:
+        >>> from calculator imprt main
+        >>> main.add(5,2)
+    
+    Args:
+        a (int): First number
+        b (int): Second number
+
+    Retunrs:
+        the sum of two nummbers 
+    """
+    return (a+b)
---
+++ docs/calculator.md
@@ -0,0 +1,7 @@
+## Calculator
+This the module that contians:
+
+* Add
+* Sub
+
+::: calculator.main
